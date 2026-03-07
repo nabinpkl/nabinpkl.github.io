c5588e3205007290b62ad06f9298a1920251944c
krunks-dev <nabin62246@gmail.com>
2026-03-07 01:53:18 -0500

    Updates

--- src/components/portfolio/FooterNote.tsx
+++
@@ -1,7 +0,0 @@
-export function FooterNote() {
-  return (
-    <footer className="footer-note">
-      <p>&copy; {new Date().getFullYear()} Nabin's Portfolio. All rights reserved.</p>
-    </footer>
-  );
-}
